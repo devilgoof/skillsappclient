56a56a166853e05469b33de608976366a7e724e8
coldapache <53959708+coldapache@users.noreply.github.com>
2020-03-23 15:27:52 -1000

    initial commit with styled components added

---
+++ endpoints-test/knexfile.js
@@ -0,0 +1,22 @@
+  
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/talented-airmen'
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
